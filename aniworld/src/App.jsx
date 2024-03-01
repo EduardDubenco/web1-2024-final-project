bdddd2b2920916d8b072d50293fee1c4e5866473
EduardDubenco <edicdubenco@gmail.com>
2024-03-01 21:42:20 +0200

    starting point ;)

---
+++ aniworld/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from "./Components/Layout-Components/Footer";
+import NavBar from "./Components/Layout-Components/NavBar";
+
+
+function App() {
+  return (
+      <>
+          <NavBar/>
+
+          <Footer/>
+      </>
+  );
+}
+
+export default App;
