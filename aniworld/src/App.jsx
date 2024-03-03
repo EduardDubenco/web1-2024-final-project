e3633026eaeb440b932e8b96a7164bbf53058d71
EduardDubenco <edicdubenco@gmail.com>
2024-03-03 16:17:40 +0200

    header v1 static no animation

--- aniworld/src/App.jsx
+++ aniworld/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from "./Components/Layout-Components/Footer";
 import NavBar from "./Components/Layout-Components/NavBar";
+import Header from "./Components/Main-Components/Header";
 
 
 function App() {
   return (
       <>
           <NavBar/>
+          <Header/>
 
           <Footer/>
       </>
--- aniworld/src/Components/Main-Components/Header.jsx
+++ aniworld/src/Components/Main-Components/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import './Header.css';
+import banner from './img/SpyXFamily-large.png';
 
 export default function Header(){
     return(
-        <>
-
-        </>
+        <section id="head">
+            <div id="bg-head">
+                <div id="img-spyx"> <img src={banner} alt=""/> </div>
+            </div>
+            <div className="additional-info">
+                <button>SPY x FAMILY New Episode</button>
+                <button>All Saints Street First Episode</button>
+                <button>My Hero Academia Season 6</button>
+                <button>BLUE LOCK</button>
+                <button>The daily Life of the Immortal King Season 3</button>
+                <button>Raven of the Inner Palace Soon</button>
+            </div>
+        </section>
     )
 }
