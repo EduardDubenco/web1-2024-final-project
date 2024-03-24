c8ae82d158b7b02a8bb1fe99abb8fc0dcad7a596
EduardDubenco <edicdubenco@gmail.com>
2024-03-24 16:53:19 +0200

    a bit of singIn and logIn page + custom in app.jsx and a bit of css

--- aniworld/src/App.jsx
+++ aniworld/src/App.jsx
@@ -2,7 +2,24 @@ import './App.css';
 import Footer from "./Components/Layout-Components/Footer";
 import NavBar from "./Components/Layout-Components/NavBar";
 import MainPage from "./Components/Pages/MainPage";
+import {createTheme} from "@mui/material/styles";
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
 
+export const CustomButton = styled(Button)`
+    background-color: #F47521;
+        &:hover {
+            background-color: #f3985b
+     }
+`;
+
+export const defaultTheme = createTheme({
+    palette: {
+        mode: 'dark',
+
+    },
+
+});
 
 function App() {
   return (
--- aniworld/src/index.js
+++ aniworld/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from "./Components/Pages/SignIn";
+import SignUp from "./Components/Pages/SignUp";
+import NavBar from "./Components/Layout-Components/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    {<SignIn/>}
+    {/*{<SignUp/>}*/}
+
   </React.StrictMode>
 );
 
